43f7320db40d7207c22113907f1660e01a124357
sophie <sneuville59@gmail.com>
2020-03-18 17:58:10 +0100

    ajout id

--- joke.js
+++ joke.js
@@ -1,4 +1,22 @@
-const imgB = document.getElementsByClassName('blague');
-imgB.addEventListener('click', ()=>{
-    const blague = document.getElementsByClassName('survol').style.diplay='block';
+/* joke dave*/
+const dave = document.getElementById("dave2");
+dave.addEventListener('click',() =>{
+  const survol = document.getElementById ('survol2').style.display = 'block';
+  })
+
+  const carambar2=  document.getElementById('carambar2');
+carambar2.addEventListener('click',() =>{
+  const survol = document.getElementById ('survol2').style.display = 'none';
 })
+
+/*joke data*/
+const data = document.getElementById("data2");
+data.addEventListener('click',() =>{
+  const survol2 = document.getElementById ('survol3').style.display = 'block';
+  })
+
+  const carambar2=  document.getElementById('carambar2');
+carambar2.addEventListener('click',() =>{
+  const survol2 = document.getElementById ('survol3').style.display = 'none';
+})
+/*joke perdue*/
