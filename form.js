59540f267badbf0e9e40a865b5b5b66b5f5e8716
Sarahshr <saraah.sahraoui@gmail.com>
2020-03-19 16:34:12 +0100

    changement couleur popup et

--- form.js
+++ form.js
@@ -4,9 +4,13 @@ document.getElementById('connect').addEventListener('click',() =>{
 });
 
 
+/*document.getElementById('BoutonQuiFerme').addEventListener('click',() =>{
+    const ClosePopup = document.getElementById('Popup').style.display = 'none';
+});
+*/
 document.getElementById('bg_h2_inscrp').addEventListener('click', ()=>{
-    const BgRegisterTitleBlack = document.getElementById('bg_h2_inscrp').style.background = 'black';
-    const BgLogTitleWhite = document.getElementById('bg_h2_conn').style.background = 'wheat';
+    const BgRegisterTitleBlack = document.getElementById('bg_h2_inscrp').style.background = 'var(--grey_color)';
+    const BgLogTitleWhite = document.getElementById('bg_h2_conn').style.background = 'white';
     const hideLogForm = document.getElementById('connexion_form').style.display = 'none';
     const PutRegisterForm = document.getElementById('inscription_form').style.display = 'flex';
     const HideCursorInRegister = document.getElementById('bg_h2_inscrp').style.cursor = 'auto';
@@ -14,10 +18,11 @@ document.getElementById('bg_h2_inscrp').addEventListener('click', ()=>{
 });
 
 document.getElementById('bg_h2_conn').addEventListener('click', ()=>{
-    const BgLogTitleBlack = document.getElementById('bg_h2_conn').style.background = 'black';
-    const BgRegisterTitleWhite = document.getElementById('bg_h2_inscrp').style.background = 'wheat';
+    const BgLogTitleBlack = document.getElementById('bg_h2_conn').style.background = 'var(--grey_color)';
+    const BgRegisterTitleWhite = document.getElementById('bg_h2_inscrp').style.background = 'white';
     const hideRegisterForm = document.getElementById('inscription_form').style.display = 'none';
     const PutLogForm = document.getElementById('connexion_form').style.display = 'flex';
     const HideCursorInLog = document.getElementById('bg_h2_conn').style.cursor = 'auto';
     const PutCursorInRegister = document.getElementById('bg_h2_inscrp').style.cursor = 'pointer';
 });
+
