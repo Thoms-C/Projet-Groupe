4c0bc2f0543829b2c4d03629a0cbba4f5e965c6b
sophie <sneuville59@gmail.com>
2020-03-19 11:54:39 +0100

    ajout js pour popup

--- joke.js
+++ joke.js
@@ -1,18 +1,21 @@
+/*faire apparaitre et disparaitre des blagues*/
 /* joke dave*/
 const dave = document.getElementById("dave2");
 dave.addEventListener('click',() =>{
-  const survol = document.getElementById ('survol2').style.display = 'block';
+  const survol = document.getElementById ('survol2').style.display = 'flex';
+  document.getElementById ('survol2').style.backgroundColor = '#ff0198';
   })
 
-  const carambar2=  document.getElementById('carambar2');
-carambar2.addEventListener('click',() =>{
+  const bouton=  document.getElementById('bouton');
+bouton.addEventListener('click',() =>{
   const survol = document.getElementById ('survol2').style.display = 'none';
 })
 
 /*joke data*/
 const data = document.getElementById("data2");
 data.addEventListener('click',() =>{
-  const survol3 = document.getElementById ('survol3').style.display = 'block';
+  const survol3 = document.getElementById ('survol3').style.display = 'flex';
+  document.getElementById ('survol3').style.backgroundColor = '#ff0198';
   })
 
 carambar2.addEventListener('click',() =>{
@@ -21,10 +24,11 @@ carambar2.addEventListener('click',() =>{
 /*joke perdue*/
 const perdue = document.getElementById("perdu2");
 perdue.addEventListener('click',() =>{
-  const survol3 = document.getElementById ('survol4').style.display = 'block';
+  const survol3 = document.getElementById ('survol4').style.display = 'flex';
+  document.getElementById ('survol4').style.backgroundColor = '#ff0198';
   })
 
 carambar2.addEventListener('click',() =>{
   const survol4 = document.getElementById ('survol4').style.display = 'none';
 })
-/*joke perdue*/
+
