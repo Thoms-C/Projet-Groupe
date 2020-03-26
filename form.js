ab8b683bfcf8d9081e425cc832d3760e0d1fd67f
sophie <sneuville59@gmail.com>
2020-03-26 19:05:19 +0100

    ajout js bouton burger

--- form.js
+++ form.js
@@ -26,3 +26,15 @@ document.getElementById('bg_h2_conn').addEventListener('click', ()=>{
     const PutCursorInRegister = document.getElementById('bg_h2_inscrp').style.cursor = 'pointer';
 });
 
+/*menu burger responsive*/
+const btn = document.getElementById("burger"); /*appelle le bouton*/
+let burgerOpen = false
+
+/*si burgerOpen faux, ouvre le burger, puis change en burgerOpen vrai donc clic suivant, ferme*/
+btn.addEventListener('click',() =>{
+  if (burgerOpen=== false){
+  const ouvreToi= document.getElementById ('ouvre').style.display = 'block';}
+  else {document.getElementById ('ouvre').style.display = 'none'; }
+    burgerOpen =! burgerOpen
+})
+
