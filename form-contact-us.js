319a143806521065774df9c921482726f22de6b0
Thoms-C <61702462+Thoms-C@users.noreply.github.com>
2020-03-26 19:08:58 +0100

    début form

---
+++ form-contact-us.js
@@ -0,0 +1,16 @@
+const ptcButton = document.getElementById('ptc');
+
+ptcButton.addEventListener('click',() => {
+    const showContactForm = document.getElementById('contactUs').style.display = "flex"
+})
+
+const submit = document.getElementById("submit");
+submit.addEventListener('click',() =>{
+  const open = document.getElementById('ourvrir').style.display = 'flex';
+  })
+
+const bouton2= document.getElementById('openit');
+bouton2.addEventListener('click',() =>{
+  document.getElementById ('ouvrir').style.display = 'none';
+  const merci = document.getElementById('')
+})
--- form.js
+++ form.js
@@ -35,3 +35,14 @@ const ptcButton = document.getElementById('ptc');
 ptcButton.addEventListener('click',() => {
     const showContactForm = document.getElementById('contactUs').style.display = "flex"
 })
+
+const submit = document.getElementById("submit");
+submit.addEventListener('click',() =>{
+  const open = document.getElementById('ourvrir').style.display = 'flex';
+  })
+
+const bouton2= document.getElementById('openit');
+bouton2.addEventListener('click',() =>{
+  document.getElementById ('ouvrir').style.display = 'none';
+  const merci = document.getElementById('')
+})
